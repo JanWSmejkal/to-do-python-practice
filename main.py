3031a6d6df224dbccc1389d065c6b7a3eecc832f
Jan Smejkal <janw.smejkal@gmail.com>
2026-03-11 17:11:51 +0100

    Learnings from day 3

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-todos = []
-
 while input != "quit":
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case "show":
-            for item in todos:
-                print(item.capitalize())
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item.capitalize()}"
+                print(row)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            todos[number] = new_todo = input("enter new todo: ")
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number)
         case "exit":
             break
 
