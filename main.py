a70c5fb474465ed8b5701a79720e64690b8a296f
Jan Smejkal <janw.smejkal@gmail.com>
2026-03-12 21:55:28 +0100

    Learnings from day 4

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ while input != "quit":
         case "add":
             todo = input("Enter a todo: ") + "\n"
 
-            file = open("todos.txt", "r")
+            file = open("files/todos.txt", "r")
             todos = file.readlines()
             file.close()
 
             todos.append(todo)
 
-            file = open("todos.txt", "w")
+            file = open("files/todos.txt", "w")
             file.writelines(todos)
             file.close()
         case "show":
-            file = open("todos.txt", "r")
+            file = open("files/todos.txt", "r")
             todos = file.readlines()
             file.close()
 
